659660ff353eaca47f78481c122c00956d176001
Fennellis <westgeze@gmail.com>
2023-09-21 16:43:26 +0300

    В классе StringArray создан метод SelectionArray

--- Program.cs
+++ Program.cs
@@ -15,4 +15,24 @@
         }
         return array;
     }
+    /// <summary>
+    /// Создает новый массив строк из элементов передаваемого массива строк, которые не более 3-х символов.
+    /// </summary>
+    /// <param name="inputArray">Передаваемый массив строк</param>
+    /// <returns>Возвращает получившийся массив строк</returns>
+    public static string[] SelectionArray(string[] inputArray)
+    {
+        string[] outputArray = Array.Empty<string>();
+        int n = 0;
+        for (int i = 0; i < inputArray.Length; i++)
+        {
+            if (inputArray[i].Length <= 3)
+            {
+                n++;
+                Array.Resize(ref outputArray, n);
+                outputArray[n - 1] = inputArray[i];
+            }
+        }
+        return outputArray;
+    }
 }
