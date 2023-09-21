fab3f8d69845a7acb7e7c4d07950a46baac7da4e
Fennellis <westgeze@gmail.com>
2023-09-21 16:42:05 +0300

    Создание класса StringArray и метода CreateStringArray

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿class StringArray
+{
+    /// <summary>
+    /// Создает массив строк длинной n
+    /// </summary>
+    /// <param name="n">Количество элементов массива</param>
+    /// <returns>Возвращает массив</returns>
+    public static string[] CreateStringArray(int n)
+    {
+        string[] array = new string[n];
+        Console.WriteLine($"Введите элементы массива({n}): ");
+        for (int i = 0; i < n; i++)
+        {
+            array[i] = Console.ReadLine()!;
+        }
+        return array;
+    }
+}
